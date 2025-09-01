e572f65058417e7a8c44ccb3a86599280567f173
veeranki <saikumarveeranki1@gmail.com>
2025-09-01 12:03:20 +0530

    initial

---
+++ src/main/java/org/ocean/leaveservice/LeaveServiceApplication.java
@@ -0,0 +1,13 @@
+package org.ocean.leaveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ocean/leaveservice/LeaveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ocean.leaveservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
